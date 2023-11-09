0d3d7e156a311acb705e3e8708ca1e326dd4ed1e
TonyaLuts <131724958+TonyaLuts@users.noreply.github.com>
2023-11-09 21:58:45 +0000

    phonebook

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,5 +1,7 @@
-@import-normalize; /* bring in normalize.css styles */
+import { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
 
+export const GlobalStyle = createGlobalStyle`
 body {
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
@@ -30,3 +32,4 @@ ul {
   margin-bottom: 0;
   padding-left: 0;
 }
+`;
